97f06671457d32f839c1254d43f801adc2a860f8
PhamQuangHuy <67786808+PhamQuangHuy-5851071028@users.noreply.github.com>
2020-07-04 17:59:32 +0700

    đổ dữ liệu vào listview

--- DatMonAnNhaHang/app/src/main/java/com/example/datmonannhahang/dat_mon.java
+++ DatMonAnNhaHang/app/src/main/java/com/example/datmonannhahang/dat_mon.java
@@ -2,9 +2,22 @@ package com.example.datmonannhahang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TabHost;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.reflect.Method;
 
 public class dat_mon extends AppCompatActivity {
 
@@ -12,12 +25,14 @@ public class dat_mon extends AppCompatActivity {
     public static int selectedTab = 1;
 
     ListView lvMonchinh, lvMonkhaivi, lvLau, lvDouong;
+    String urlGetDataMonChinh = "http://localhost/nhahang/getdataMonChinh.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dat_mon);
         showTabHost();
         addViews();
+        getdataMonChinh(urlGetDataMonChinh);
     }
 
     private void addViews() {
@@ -50,5 +65,23 @@ public class dat_mon extends AppCompatActivity {
         tab2.setContent(R.id.lau);
         tab2.setIndicator("Đồ uống");
         tabHost.addTab(tab4);
+    }
+    private void getdataMonChinh(String url){
+        RequestQueue requestQueue = Volley.newRequestQueue(dat_mon.this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url,
+                null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(jsonArrayRequest);
     }
 }
